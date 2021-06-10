60a8d1188a0fd3f48557391bcf77d79c5efb719c
MatthiasBiswas <49491024+MatthiasBiswas@users.noreply.github.com>
2021-06-10 14:17:55 +0600

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 10 13:17:54 2021
+
+@author: matth
+"""
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Python is {adj}! It makes me so excited all the time because \
+I love to {verb1}. Continue programming to {verb2} like you are {famous_person}!"
+
+print(madlib)
